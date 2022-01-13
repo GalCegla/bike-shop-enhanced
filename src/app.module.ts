7d2f53b20abe6cf0de642ab49364684ee9e1f3c3
GalCegla <galcegla@gmail.com>
2022-01-13 17:48:45 +0200

    Add module dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { UserService } from './users/user.service';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [AppController, UsersController],
+  providers: [AppService, PrismaService, UserService],
 })
 export class AppModule {}

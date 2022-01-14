d983438468777e9bda340f4a7f18730b88652dbe
GalCegla <galcegla@gmail.com>
2022-01-14 15:30:06 +0200

    Extract free strings

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
 import { ValidationPipe } from './validation.pipe';
 
+const SWAGGER_TITLE = "Gal's Bike Shop";
+const SWAGGER_DESCRIPTION = 'Rent Bikes!';
+const SWAGGER_VERSION = '1.0';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -13,9 +17,9 @@ async function bootstrap() {
   prismaService.enableShutdownHooks(app);
 
   const config = new DocumentBuilder()
-    .setTitle("Gal's Bike Shop")
-    .setDescription('Rent Bikes!')
-    .setVersion('1.0')
+    .setTitle(SWAGGER_TITLE)
+    .setDescription(SWAGGER_DESCRIPTION)
+    .setVersion(SWAGGER_VERSION)
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);

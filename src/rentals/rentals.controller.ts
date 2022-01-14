b72d1a774e15fb9e8e9d3137d40e9e662e1f2984
GalCegla <galcegla@gmail.com>
2022-01-14 18:42:30 +0200

    Document rentals

--- src/rentals/rentals.controller.ts
+++ src/rentals/rentals.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Param, Put } from '@nestjs/common';
 import { RentalService } from './rental.service';
 import { Rental as RentalModel } from '@prisma/client';
 import { UpdateRentalDTO } from './dto/update-rental.dto';
+import { ApiBearerAuth, ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('rentals')
 export class RentalsController {
@@ -9,16 +10,20 @@ export class RentalsController {
   constructor(private rentalService: RentalService) {}
 
   @Get(':id')
+  @ApiOkResponse({ description: 'Get a rental with an ID' })
   async getRentalById(@Param('id') id: string): Promise<RentalModel> {
     return this.rentalService.rental({ id });
   }
 
   @Get()
+  @ApiOkResponse({ description: 'Get all rentals' })
+  @ApiBearerAuth()
   async getAllRentals(): Promise<RentalModel[]> {
     return this.rentalService.rentals({});
   }
 
   @Put(':id')
+  @ApiOkResponse({ description: 'Update a rental using a rental ID' })
   async updateRental(
     @Param('id') id: string,
     @Body() updateRentalDto: UpdateRentalDTO,
@@ -30,6 +35,7 @@ export class RentalsController {
   }
 
   @Delete(':id')
+  @ApiOkResponse({ description: 'Delete a rental using a rental ID' })
   async deleteRental(@Param('id') id: string): Promise<RentalModel> {
     return this.rentalService.deleteRental({ id });
   }

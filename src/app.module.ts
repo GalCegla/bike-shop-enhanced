8fb9a55dda1abe36b04735bb418253cbf3bd4a43
GalCegla <galcegla@gmail.com>
2022-01-14 15:01:28 +0200

    Add rentals module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BikesModule } from './bikes/bikes.module';
+import { RentalsModule } from './rentals/rentals.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, BikesModule],
+  imports: [UsersModule, BikesModule, RentalsModule],
   controllers: [AppController],
   providers: [AppService],
 })
